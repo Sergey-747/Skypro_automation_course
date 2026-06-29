9a3db55f3c3ec2e95b3784e80a7dc6db52c3286f
Sergey-747 <serfre13@gmail.com>
2026-06-29 17:33:44 +0700

    Update lesson_3_task_2.py

--- 03_lesson/lesson_3_task_2.py
+++ 03_lesson/lesson_3_task_2.py
@@ -3,7 +3,7 @@ from smartphone import Smartphone
 catalog = [
     Smartphone("Xiaomi", "Xiaomi 15 Pro", "+79132334567" ), 
     Smartphone("Asus", "Asus ROG Phone 9 Pro", "+79912345678"),
-    Smartphone("Appole", "Appole 12", "+79132334567" ), 
+    Smartphone("Apple", "Apple 12", "+79132334567" ), 
     Smartphone("Xiaomi", "Xiaomi Redmi 10C", "+79138544567"),
     Smartphone("OPPO", "OPPO Find X9 Pro", "+79912348756"),
     Smartphone("CMF Phone", "CMF Phone 2 Pro", "+79036812345")
