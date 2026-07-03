5a9b8cac793c40e794b3fad1d372809acb936dde
Sergey-747 <serfre13@gmail.com>
2026-07-03 17:59:39 +0700

    Добавлены тесты

--- 05_lesson/test_lesson05_task3.py
+++ 05_lesson/test_lesson05_task3.py
@@ -10,8 +10,15 @@ driver.maximize_window()
 def test_paig():
     driver.get("https://httpbin.org/links/10")
     #Находим все ссылки на странице (тег <a>).
-    link = driver.find_elements(By.TAG_NAME, "a")
-    print(f"Найдено ссылок на странице: {len(link)}")
+    links = driver.find_elements(By.TAG_NAME, "a")
+    print(f"Найдено ссылок на странице: {len(links)}")
+        # Проверьте, что количество ссылок равно 9.
+    assert len(links) == 9, "Ссылок не 9"
+      # Проверяем, что все ссылки отображаются
+    for link in links:
+        assert link.is_displayed(), "Не все ссылки отображаются"
+      # Проверяем текст первой ссылки
+    assert "1" in links[0].text, "Это не Первая ссылка "
 
 test_paig()
 driver.quit()
